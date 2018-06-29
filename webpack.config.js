5aad8ea542860ac2a49b32cdea15e11da8b003a3
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2018-06-29 13:21:37 +0200

    Add webpack-config template

---
+++ webpack.config.js
@@ -0,0 +1,133 @@
+// Template for webpack.config.js in Fable projects
+// Find latest version in https://github.com/fable-compiler/webpack-config-template
+
+// In most cases, you'll only need to edit the CONFIG object
+// See below if you need better fine-tuning of Webpack options
+
+var CONFIG = {
+    indexHtmlTemplate: "./src/index.html",
+    fsharpEntry: "./src/App.fsproj",
+    cssEntry: "./src/style.sass",
+    outputDir: "./deploy",
+    assetsDir: "./public",
+    devServerPort: 8080,
+    devServerProxy: undefined,
+    // Use babel-preset-env to generate JS compatible with most-used browsers.
+    // More info at https://github.com/babel/babel/blob/master/packages/babel-preset-env/README.md
+    babel: {
+        presets: [
+            ["env", {
+                "modules": false,
+                "useBuiltIns": "usage",
+            }]
+        ],
+    }
+}
+
+// If we're running the webpack-dev-server, assume we're in development mode
+var isProduction = !process.argv.find(v => v.indexOf('webpack-dev-server') !== -1);
+console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
+
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+// The HtmlWebpackPlugin allows us to use a template for the index.html page
+// and automatically injects <script> or <link> tags for generated bundles.
+var commonPlugins = [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: CONFIG.indexHtmlTemplate
+    })
+];
+
+module.exports = {
+    entry: CONFIG.fsharpEntry,
+    // NOTE we add a hash to the output file name in production
+    // to prevent browser caching if code changes
+    output: {
+        path: path.join(__dirname, CONFIG.outputDir),
+        filename: isProduction ? '[name].[hash].js' : '[name].js'
+    },
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    optimization: {
+        // Split the code coming from npm packages into a different file.
+        // 3rd party dependencies change less often, let the browser cache them.
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /node_modules/,
+                    name: "vendors",
+                    chunks: "all"
+                }
+            }
+        },
+    },
+    // Besides the HtmlPlugin, we use the following plugins:
+    // PRODUCTION
+    //      - MiniCssExtractPlugin: Extracts CSS from bundle to a different file
+    //      - CopyWebpackPlugin: Copies static assets to output directory
+    // DEVELOPMENT
+    //      - HotModuleReplacementPlugin: Enables hot reloading when code changes without refreshing
+    plugins: isProduction ?
+        commonPlugins.concat([
+            new MiniCssExtractPlugin({ filename: 'style.css' }),
+            new CopyWebpackPlugin([{ from: CONFIG.assetsDir }]),
+            // Inlining is causing problems in minified code
+            // See https://github.com/mishoo/UglifyJS2/issues/2842#issuecomment-359527962
+            new UglifyJSPlugin({
+                uglifyOptions: {
+                    compress: { inline: false }
+                }
+            }),
+        ])
+        : commonPlugins.concat([
+            new webpack.HotModuleReplacementPlugin(),
+        ]),
+    // Configuration for webpack-dev-server
+    devServer: {
+        publicPath: "/",
+        contentBase: CONFIG.assetsDir,
+        port: CONFIG.devServerPort,
+        proxy: CONFIG.devServerProxy,
+        hot: true,
+        inline: true
+    },
+    // - fable-loader: transforms F# into JS
+    // - babel-loader: transforms JS to old syntax (compatible with old browsers)
+    // - sass-loaders: transforms SASS/SCSS into JS
+    // - file-loader: Moves files referenced in the code (fonts, images) into output folder
+    module: {
+        rules: [
+            {
+                test: /\.fs(x|proj)?$/,
+                use: "fable-loader"
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: CONFIG.babel
+                },
+            },
+            {
+                test: /\.(sass|scss|css)$/,
+                use: [
+                    isProduction ? MiniCssExtractPlugin.loader :
+                        'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*)?$/,
+                use: ["file-loader"]
+            }
+        ]
+    }
+};
