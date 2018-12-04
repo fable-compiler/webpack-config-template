b83e225d135b340b72214fdaee2d49aa47994a8a
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2018-12-04 11:23:08 +0100

    Add note about minifying CSS

--- webpack.config.js
+++ webpack.config.js
@@ -87,6 +87,7 @@ module.exports = {
     // Besides the HtmlPlugin, we use the following plugins:
     // PRODUCTION
     //      - MiniCssExtractPlugin: Extracts CSS from bundle to a different file
+    //          To minify CSS, see https://github.com/webpack-contrib/mini-css-extract-plugin#minimizing-for-production    
     //      - CopyWebpackPlugin: Copies static assets to output directory
     // DEVELOPMENT
     //      - HotModuleReplacementPlugin: Enables hot reloading when code changes without refreshing
