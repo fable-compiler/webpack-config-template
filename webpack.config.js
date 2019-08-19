5aa0b318ea758d1d27cd146d5df9f3d897836e83
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2019-08-19 12:11:05 +0200

    Use .scss instead of .sass by default

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ var CONFIG = {
     // See https://github.com/jantimon/html-webpack-plugin
     indexHtmlTemplate: './src/index.html',
     fsharpEntry: './src/App.fsproj',
-    cssEntry: './src/style.sass',
+    cssEntry: './src/style.scss',
     outputDir: './deploy',
     assetsDir: './public',
     devServerPort: 8080,
