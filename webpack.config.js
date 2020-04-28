5bcb4fc7ed2d4db17057f16d8cc5efc606691c4e
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2020-04-28 17:08:22 +0900

    Cover more options: - publicPath - json stats - contenthash instead of just hash - non-hash client-side routing

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ var CONFIG = {
     cssEntry: './src/style.scss',
     outputDir: './deploy',
     assetsDir: './public',
+    publicPath: '/', // Where the bundled files are accessible relative to server root
     devServerPort: 8080,
     // When using webpack-dev-server, you may need to redirect some calls
     // to a external API server. See https://webpack.js.org/configuration/dev-server/#devserver-proxy
@@ -40,9 +41,13 @@ var CONFIG = {
     }
 }
 
-// If we're running the webpack-dev-server, assume we're in development mode
-var isProduction = !process.argv.find(v => v.indexOf('webpack-dev-server') !== -1);
-console.log('Bundling for ' + (isProduction ? 'production' : 'development') + '...');
+// If we're running webpack-dev-server, assume we're in development
+var isProduction = !hasArg(/webpack-dev-server/);
+var outputWebpackStatsAsJson = hasArg('--json');
+
+if (!outputWebpackStatsAsJson) {
+    console.log("Bundling CLIENT for " + (isProduction ? "production" : "development") + "...");
+}
 
 // The HtmlWebpackPlugin allows us to use a template for the index.html page
 // and automatically injects <script> or <link> tags for generated bundles.
@@ -61,14 +66,15 @@ module.exports = {
     entry: isProduction ? {
         app: [resolve(CONFIG.fsharpEntry), resolve(CONFIG.cssEntry)]
     } : {
-            app: [resolve(CONFIG.fsharpEntry)],
-            style: [resolve(CONFIG.cssEntry)]
-        },
+        app: [resolve(CONFIG.fsharpEntry)],
+        style: [resolve(CONFIG.cssEntry)]
+    },
     // Add a hash to the output file name in production
     // to prevent browser caching if code changes
     output: {
+        publicPath: CONFIG.publicPath,        
         path: resolve(CONFIG.outputDir),
-        filename: isProduction ? '[name].[hash].js' : '[name].js'
+        filename: isProduction ? '[name].[contenthash].js' : '[name].js',
     },
     mode: isProduction ? 'production' : 'development',
     devtool: isProduction ? 'source-map' : 'eval-source-map',
@@ -86,7 +92,7 @@ module.exports = {
     //      - HotModuleReplacementPlugin: Enables hot reloading when code changes without refreshing
     plugins: isProduction ?
         commonPlugins.concat([
-            new MiniCssExtractPlugin({ filename: 'style.[hash].css' }),
+            new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
             new CopyWebpackPlugin([{ from: resolve(CONFIG.assetsDir) }]),
         ])
         : commonPlugins.concat([
@@ -98,7 +104,13 @@ module.exports = {
     },
     // Configuration for webpack-dev-server
     devServer: {
-        publicPath: '/',
+        // Necessary when using non-hash client-side routing
+        // This assumes the index.html is accessible from server root
+        // For more info, see https://webpack.js.org/configuration/dev-server/#devserverhistoryapifallback
+        historyApiFallback: {
+            index: '/'
+        },
+        publicPath: CONFIG.publicPath,
         contentBase: resolve(CONFIG.assetsDir),
         host: '0.0.0.0',
         port: CONFIG.devServerPort,
@@ -117,7 +129,8 @@ module.exports = {
                 use: {
                     loader: 'fable-loader',
                     options: {
-                        babel: CONFIG.babel
+                        babel: CONFIG.babel,
+                        silent: outputWebpackStatsAsJson,
                     }
                 }
             },
@@ -136,12 +149,10 @@ module.exports = {
                         ? MiniCssExtractPlugin.loader
                         : 'style-loader',
                     'css-loader',
+                    'resolve-url-loader',
                     {
-                        loader: 'resolve-url-loader',
-                    },
-                    {
-                      loader: 'sass-loader',
-                      options: { implementation: require('sass') }
+                        loader: 'sass-loader',
+                        options: { implementation: require('sass') }
                     }
                 ],
             },
@@ -155,4 +166,10 @@ module.exports = {
 
 function resolve(filePath) {
     return path.isAbsolute(filePath) ? filePath : path.join(__dirname, filePath);
+}
+
+function hasArg(arg) {
+    return arg instanceof RegExp
+        ? process.argv.some(x => arg.test(x))
+        : process.argv.indexOf(arg) !== -1;
 }
